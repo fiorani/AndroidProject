f08d597c765dbf40b08a013a2464039aa32bf1a1
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-06-03 21:43:17 +0200

    fix image cro

--- EatIt/app/src/main/java/com/example/eatit/ui/HomeScreen.kt
+++ EatIt/app/src/main/java/com/example/eatit/ui/HomeScreen.kt
@@ -4,12 +4,15 @@ import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -126,7 +129,8 @@ fun RestaurantCard(
                         .build(),
                     contentDescription = "image of the restaurant",
                     modifier = Modifier
-                        .size(size = 100.dp)
+                        .size(size = 100.dp).clip(CircleShape),
+                    contentScale = ContentScale.Crop,
                 )
             }
             Column(
