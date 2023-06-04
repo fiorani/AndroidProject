be2f693d6af168d83c74549b66097053a31364c5
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-06-04 19:26:29 +0200

    fix user profile

--- EatIt/app/src/main/java/com/example/eatit/ui/UserProfileScreen.kt
+++ EatIt/app/src/main/java/com/example/eatit/ui/UserProfileScreen.kt
@@ -5,11 +5,15 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -17,6 +21,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,11 +34,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.example.eatit.R
+import com.example.eatit.ui.components.EatItImage
 import com.example.eatit.viewModel.UsersViewModel
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.DocumentSnapshot
@@ -79,41 +87,31 @@ fun UserProfileScreen(modifier: Modifier = Modifier, usersViewModel: UsersViewMo
         }
     Scaffold { innerPadding ->
         Column(modifier.padding(innerPadding)) {
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
-            ) {
-                Row {
-                    if (user.size > 0) {
-                        if (user[0].data?.get("photo").toString() != "") {
-                            AsyncImage(
-                                model = ImageRequest.Builder(LocalContext.current)
-                                    .data(Uri.parse(user[0].data?.get("photo").toString()))
-                                    .crossfade(true)
-                                    .build(),
-                                contentDescription = "image of the restaurant",
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .height(200.dp)
-                            )
-                        }
-                        Column {
-                            Text(
-                                text = user[0].data!!["userName"].toString(),
-                                modifier = Modifier.padding(8.dp),
-                                fontSize = 32.sp
-                            )
-                            Text(
-                                text = user[0].data!!["userName"].toString(),
-                                modifier = Modifier.padding(8.dp),
-                                fontSize = 16.sp
-                            )
-                        }
+            if(user.size > 0){
+                Box(modifier = Modifier.fillMaxWidth()) {
+                    EatItImage(user[0].data?.get("photo").toString())
+                    Column(
+                        verticalArrangement = Arrangement.Bottom,
+                        horizontalAlignment = Alignment.Start,
+                        modifier = Modifier
+                            .height(200.dp)) {
+                        Text(
+                            text = user[0].data!!["userName"].toString(),
+                            color = MaterialTheme.colorScheme.background,
+                            style = MaterialTheme.typography.titleLarge,
 
+                            )
+                        Spacer(modifier = Modifier.size(15.dp))
+                        Text(
+                            text = user[0].data!!["userName"].toString(),
+                            color = MaterialTheme.colorScheme.background,
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                        Spacer(modifier = Modifier.size(15.dp))
                     }
                 }
             }
+
             Text(
                 text = "Order list:",
                 modifier = Modifier.padding(8.dp),
