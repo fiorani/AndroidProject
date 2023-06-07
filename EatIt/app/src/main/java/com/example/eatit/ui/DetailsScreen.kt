1902b7a8e34f6792b50dad95064bb20eeb58785a
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-06-07 23:13:31 +0200

    redesign settings

--- EatIt/app/src/main/java/com/example/eatit/ui/DetailsScreen.kt
+++ EatIt/app/src/main/java/com/example/eatit/ui/DetailsScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -128,7 +129,7 @@ fun DetailsRestaurantScreen(
                     Text(
                         text = restaurant?.name.toString(),
                         style = MaterialTheme.typography.titleLarge,
-
+                        color = Color.White
                         )
                     Spacer(modifier = Modifier.size(15.dp))
                     Row(verticalAlignment = Alignment.CenterVertically,
--- EatIt/app/src/main/java/com/example/eatit/ui/UserProfileScreen.kt
+++ EatIt/app/src/main/java/com/example/eatit/ui/UserProfileScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -73,12 +74,13 @@ fun UserProfileScreen(
                             Text(
                                 text = user.name.toString(),
                                 style = MaterialTheme.typography.titleLarge,
-
+                                color = Color.White
                                 )
                             Spacer(modifier = Modifier.size(15.dp))
                             Text(
                                 text = user.position.toString(),
-                                style = MaterialTheme.typography.titleMedium
+                                style = MaterialTheme.typography.titleMedium,
+                                color = Color.White
                             )
                             Spacer(modifier = Modifier.size(15.dp))
                         }
