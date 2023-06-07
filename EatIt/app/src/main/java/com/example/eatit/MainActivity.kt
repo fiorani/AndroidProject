52d35d96a1ff26574c8f97565c6f0e2cbdb7b96c
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-06-07 03:22:47 +0200

    correct algorthm

--- EatIt/app/src/main/java/com/example/eatit/MainActivity.kt
+++ EatIt/app/src/main/java/com/example/eatit/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.eatit
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
@@ -11,13 +13,15 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
-import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
@@ -28,9 +32,12 @@ import com.example.eatit.ui.theme.EatItTheme
 import com.example.eatit.viewModel.UsersViewModel
 import com.example.eatit.viewModel.WarningViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.Granularity
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -38,21 +45,37 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private lateinit var auth: FirebaseAuth
-    private lateinit var locationCallback: LocationCallback
-    private var queue: RequestQueue? = null
-    private val warningViewModel by viewModels<WarningViewModel>()
-    private val REQUESTING_LOCATION_UPDATES_KEY = "REQUESTING_LOCATION_UPDATES_KEY"
-    private var requestingLocationUpdates = false
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var auth: FirebaseAuth
+    private lateinit var locationPermissionRequest: ActivityResultLauncher<String>
     private lateinit var networkCallback: ConnectivityManager.NetworkCallback
     private lateinit var connectivityManager: ConnectivityManager
-    var location = mutableStateOf(LocationDetails(0.toDouble(), 0.toDouble()))
+    private var requestingLocationUpdates = mutableStateOf(false)
+    private var queue: RequestQueue? = null
+    val location = mutableStateOf(LocationDetails(0.toDouble(), 0.toDouble()))
+    val warningViewModel by viewModels<WarningViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = Firebase.auth
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        connectivityManager =
+            applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+       locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                startLocationUpdates()
+            } else {
+                warningViewModel.setPermissionSnackBarVisibility(true)
+            }
+        }
+        locationRequest =
+            LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 10_000).apply {
+                setGranularity(Granularity.GRANULARITY_PERMISSION_LEVEL)
+            }.build()
         locationCallback = object : LocationCallback() {
             override fun onLocationResult(p0: LocationResult) {
                 super.onLocationResult(p0)
@@ -60,7 +83,6 @@ class MainActivity : ComponentActivity() {
                     p0.locations.first().latitude,
                     p0.locations.first().longitude
                 )
-                stopLocationUpdates()
                 if (isOnline(connectivityManager = connectivityManager)) {
                     sendRequest(location.value, connectivityManager)
                 } else {
@@ -68,18 +90,19 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        updateValuesFromBundle(savedInstanceState)
         networkCallback = object : ConnectivityManager.NetworkCallback() {
             override fun onAvailable(network: Network) {
-                if (requestingLocationUpdates) {
+                if (requestingLocationUpdates.value) {
                     sendRequest(location.value, connectivityManager)
                     warningViewModel.setConnectivitySnackBarVisibility(false)
                 }
             }
+
             override fun onLost(network: Network) {
                 warningViewModel.setConnectivitySnackBarVisibility(true)
             }
         }
+
         setContent {
             EatItTheme {
                 // A surface container using the 'background' color from the theme
@@ -92,48 +115,121 @@ class MainActivity : ComponentActivity() {
                         warningViewModel = warningViewModel,
                         signIn = ::signIn,
                         createAccount = ::createAccount,
-                        startLocationUpdates = ::startLocationUpdates,
+                        sendRequest =  ::startLocationUpdates
                     )
                 }
-                if (requestingLocationUpdates) {
+                if (requestingLocationUpdates.value) {
                     connectivityManager.registerDefaultNetworkCallback(networkCallback)
                 }
             }
         }
     }
-    private fun updateValuesFromBundle(savedInstanceState: Bundle?) {
-        savedInstanceState ?: return
-        // Update the value of requestingLocationUpdates from the Bundle.
-
-        if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
-            requestingLocationUpdates = savedInstanceState.getBoolean(
-                REQUESTING_LOCATION_UPDATES_KEY)
-        }
+ fun startRequest(){
+        requestingLocationUpdates.value = true
     }
+    private fun sendRequest(location: LocationDetails, connectivityManager: ConnectivityManager) {
+            val userViewModel by viewModels<UsersViewModel>()
+            queue = Volley.newRequestQueue(this)
+            val url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+                    +location.latitude +","+location.longitude+"&key=AIzaSyAtkgSO0EAakNnErsYTuO1ORfA4QFsnqiw"
+            val jsonObjectRequest = JsonObjectRequest(
+                Request.Method.GET, url, null,
+                { response ->
+                    val results = response.getJSONArray("results")
+                    if (results.length() > 0) {
+                        val address = results.getJSONObject(0).getString("formatted_address")
+                        Log.d("MAINACTIVITY-SENDREQUEST", address)
+                        userViewModel.setPosition(address)
+                    }
+                    connectivityManager.unregisterNetworkCallback(networkCallback)
+                    requestingLocationUpdates.value = false
+                },
+                { error ->
+                    Log.d("MAINACTIVITY-SENDREQUEST", error.toString())
+                }
+            )
+            jsonObjectRequest.tag = TAG
+            queue?.add(jsonObjectRequest)
+
+    }
+
     override fun onResume() {
         super.onResume()
-        if (requestingLocationUpdates) startLocationUpdates()
+        if (requestingLocationUpdates.value) {
+        startLocationUpdates()
+        }
+    }
 
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
     }
-    fun sendRequest(location: LocationDetails, connectivityManager: ConnectivityManager) {
-        val userViewModel by viewModels<UsersViewModel>()
-        queue = Volley.newRequestQueue(this)
-        val url = "https://nominatim.openstreetmap.org/reverse?lat=" + location.latitude +
-                "&lon=" + location.longitude + "&format=jsonv2&limit=1"
-        val jsonObjectRequest = JsonObjectRequest(
-            Request.Method.GET, url, null,
-            { response ->
-                userViewModel.setPosition(response.getString("display_name"))
-                connectivityManager.unregisterNetworkCallback(networkCallback)
-                requestingLocationUpdates = false
-            },
-            { error ->
-                Log.d("MAINACTIVITY-SENDREQUEST", error.toString())
+
+    override fun onStop() {
+        super.onStop()
+        queue?.cancelAll(TAG)
+        if (requestingLocationUpdates.value ) {
+            (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
+                .unregisterNetworkCallback(networkCallback)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (requestingLocationUpdates.value)
+            (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
+                .registerDefaultNetworkCallback(networkCallback)
+    }
+
+    private fun startLocationUpdates() {
+        requestingLocationUpdates.value = true
+        val permission = Manifest.permission.ACCESS_COARSE_LOCATION
+        when {
+            //permission already granted
+            ContextCompat.checkSelfPermission(
+                this,
+                permission
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                locationRequest =
+                    LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 10_000).apply {
+                        setGranularity(Granularity.GRANULARITY_PERMISSION_LEVEL)
+                        setWaitForAccurateLocation(true)
+                    }.build()
+
+                val gpsEnabled = checkGPS()
+                if (gpsEnabled) {
+                    fusedLocationProviderClient.requestLocationUpdates(
+                        locationRequest,
+                        locationCallback,
+                        Looper.getMainLooper()
+                    )
+                } else {
+                    warningViewModel.setGPSAlertDialogVisibility(true)
+                }
             }
-        )
-        jsonObjectRequest.tag = TAG
-        queue?.add(jsonObjectRequest)
+            //permission already denied
+            shouldShowRequestPermissionRationale(permission) -> {
+                warningViewModel.setPermissionSnackBarVisibility(true)
+            }
+
+            else -> {
+                //first time: ask for permissions
+                locationPermissionRequest.launch(
+                    permission
+                )
+            }
+        }
     }
+
+    private fun stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+
+    private fun checkGPS(): Boolean {
+        val mLocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+
     private fun isOnline(connectivityManager: ConnectivityManager): Boolean {
         val capabilities =
             connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
@@ -143,42 +239,6 @@ class MainActivity : ComponentActivity() {
             return true
         }
         return false
-    }
-
-    private fun startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return
-        }
-        fusedLocationClient.requestLocationUpdates(locationRequest,
-            locationCallback,
-            Looper.getMainLooper())
-    }
-    override fun onPause() {
-        super.onPause()
-        stopLocationUpdates()
-
-    }
-    private fun stopLocationUpdates() {
-        fusedLocationClient.removeLocationUpdates(locationCallback)
-    }
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY, requestingLocationUpdates)
-            super.onSaveInstanceState(outState)
-    }
-    override fun onStop() {
-        super.onStop()
-        queue?.cancelAll(TAG)
-    }
-
-    override fun onStart() {
-        super.onStart()
     }
 
     companion object {
@@ -248,4 +308,5 @@ class MainActivity : ComponentActivity() {
         ).show()
     }
 }
+
 
--- EatIt/app/src/main/java/com/example/eatit/ui/MapScreen.kt
+++ EatIt/app/src/main/java/com/example/eatit/ui/MapScreen.kt
@@ -1,56 +1,67 @@
 package com.example.eatit.ui
 
+import android.Manifest
 import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.lifecycleScope
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat
 import com.example.eatit.model.Restaurant
 import com.example.eatit.viewModel.RestaurantsViewModel
 import com.example.eatit.viewModel.UsersViewModel
+import com.google.android.gms.location.Granularity
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.LocationSource
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-
 
 @Composable
 fun MapScreen(
-
     restaurantsViewModel: RestaurantsViewModel,
-    modifier: Modifier = Modifier,
-    usersViewModel: UsersViewModel
+    usersViewModel: UsersViewModel,
+    modifier: Modifier = Modifier
 ) {
     val context = LocalContext.current
     var restaurants by remember { mutableStateOf<List<Restaurant>>(emptyList()) }
     val markers = remember { mutableStateListOf<MarkerInfo>() }
-    LaunchedEffect(Unit) {
+   /* LaunchedEffect(Unit) {
         restaurants = restaurantsViewModel.getRestaurants()
     }
     LaunchedEffect(restaurants) {
@@ -69,28 +80,63 @@ fun MapScreen(
                 )
             }
         }
-    }
-
-    var myPosition by rememberSaveable { mutableStateOf("") }
-    var cameraPositionState = rememberCameraPositionState {
+    }*/
+    val cameraPositionState = rememberCameraPositionState {
         CameraPosition.fromLatLngZoom(LatLng(0.0, 0.0), 10f)
     }
-    myPosition = usersViewModel.userPosition.value
-    val currentLocation = Geocoder(context).getFromLocationName(myPosition, 1)
-    if (currentLocation != null && currentLocation.size > 0) {
-        cameraPositionState = rememberCameraPositionState {
-            position = CameraPosition.fromLatLngZoom(
-                LatLng(currentLocation[0].latitude, currentLocation[0].longitude), 9f
-            )
+    val locationSource = MyLocationSource()
+    var currentLocation by remember { mutableStateOf(Location("MyLocationProvider")) }
+    val locationRequest = LocationRequest.Builder(10_000)
+        .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+        .setMinUpdateIntervalMillis(10_000)
+        .setMinUpdateDistanceMeters(10.0f)
+        .setWaitForAccurateLocation(true)
+        .build()
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            super.onLocationResult(p0)
+            currentLocation = p0.lastLocation!!
         }
     }
+    if (ActivityCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED
+    ) {
+        // TODO: Consider calling
+        //    ActivityCompat#requestPermissions
+        // here to request the missing permissions, and then overriding
+        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+        //                                          int[] grantResults)
+        // to handle the case where the user grants the permission. See the documentation
+        // for ActivityCompat#requestPermissions for more details.
+        return
+    }
+    LocationServices.getFusedLocationProviderClient(context).requestLocationUpdates(
+        locationRequest,
+        locationCallback,
+        Looper.getMainLooper()
+    )
+    LaunchedEffect(currentLocation) {
+        Log.d(TAG, "Updating blue dot on map...")
+        locationSource.onLocationChanged(currentLocation)
+        Log.d(TAG, "Updating camera position...")
+        /*val cameraPosition = CameraPosition.fromLatLngZoom(
+            LatLng(currentLocation.latitude, currentLocation.longitude), 20f)
+        cameraPositionState.animate(CameraUpdateFactory.newCameraPosition(cameraPosition), 1_000)*/
+    }
 
+    val mapProperties by remember { mutableStateOf(MapProperties(isMyLocationEnabled = true)) }
     Scaffold { innerPadding ->
         Column(modifier.padding(innerPadding)) {
             GoogleMap(
                 modifier = Modifier.fillMaxSize(),
                 cameraPositionState = cameraPositionState,
-                //locationSource = locationSource,
+                locationSource = locationSource,
+                properties  = mapProperties
             ) {
                 markers.forEach { markerInfo ->
                     Marker(
@@ -109,8 +155,9 @@ data class MarkerInfo(
     val title: String,
     val position: LatLng
 )
-private class MyLocationSource : LocationSource {
 
+@Stable
+class MyLocationSource : LocationSource {
     private var listener: LocationSource.OnLocationChangedListener? = null
 
     override fun activate(listener: LocationSource.OnLocationChangedListener) {
