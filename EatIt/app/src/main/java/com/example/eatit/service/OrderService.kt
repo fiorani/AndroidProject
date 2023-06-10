e7f11a718e44301328c61f46035ff64b2f4bea73
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-06-10 17:26:58 +0200

    add notification

--- EatIt/app/src/main/java/com/example/eatit/service/OrderService.kt
+++ EatIt/app/src/main/java/com/example/eatit/service/OrderService.kt
@@ -1,6 +1,6 @@
 package com.example.eatit.service
 
-import android.app.Notification
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -11,31 +11,33 @@ import android.content.Intent
 import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.eatit.MainActivity
 import com.example.eatit.R
 import com.example.eatit.model.Order
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ListenerRegistration
-import com.google.firebase.firestore.Query
 
 class OrderService : Service() {
-    private lateinit var ordersListenerRegistration: ListenerRegistration
-    private lateinit var notificationManager: NotificationManager
-    private val channelId = "OrderUpdatesChannel"
-    private val channelName = "Order Updates"
-
-    override fun onCreate() {
-        super.onCreate()
-        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        createNotificationChannel()
-    }
-
+    private val notificationId = 101
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        createNotificationChannel()
         startOrdersListener()
         return START_STICKY
     }
-
+    private fun createNotificationChannel() {
+        val name =  "OrderChannel"
+        val descriptionText =  "OrderChannel"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel("OrderChannel", name, importance).apply {
+            description = descriptionText
+        }
+        // Register the channel with the system
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
@@ -43,42 +45,41 @@ class OrderService : Service() {
     private fun startOrdersListener() {
         FirebaseFirestore.getInstance().collection("orders")
             .whereEqualTo("userId", FirebaseAuth.getInstance().currentUser?.uid)
-            .addSnapshotListener { snapshot, e ->
+            .addSnapshotListener { snapshots, e ->
                 if (e != null) {
                     Log.w(TAG, "Listen failed.", e)
                     return@addSnapshotListener
                 }
-                if (snapshot != null) {
-                    Log.d(TAG, "Current data: ${snapshot.documents}")
-                    sendOrderUpdateNotification()
+                if (snapshots != null) {
+                    for (dc in snapshots!!.documentChanges) {
+                        if (dc.type == DocumentChange.Type.MODIFIED) {
+                            Log.d(TAG, "Modify: ${dc.document.data}")
+                            sendNotification(dc.document.toObject(Order::class.java))
+                        }
+                    }
+
                 } else {
                     Log.d(TAG, "Current data: null")
                 }
+            }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun sendNotification(order: Order) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
-    }
-
-    private fun sendOrderUpdateNotification() {
-        val intent = Intent(this, MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(
-            this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-        )
-        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        val builder = NotificationCompat.Builder(this, "OrderChannel")
             .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle("Ordine Aggiornato")
-            .setContentText("Uno o più ordini sono stati aggiornati.")
+            .setContentTitle("Order status changed")
+            .setContentText("status:"+order.status)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            // Set the intent that will fire when the user taps the notification
             .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
-
-        val notificationId = 1
-        notificationManager.notify(notificationId, notificationBuilder.build())
+            with(NotificationManagerCompat.from(this)) {
+                notify(notificationId, builder.build())
+            }
     }
 
-    private fun createNotificationChannel() {
-        val channel = NotificationChannel(
-            channelId,
-            channelName,
-            NotificationManager.IMPORTANCE_DEFAULT
-        )
-        notificationManager.createNotificationChannel(channel)
-    }
 }
